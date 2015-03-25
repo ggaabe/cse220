7897157e6d343b6a4d6f1d0b186f0ad59954b0f4
Gabriel Garrett <ggaabe@gmail.com>
2015-03-25 19:53:06 -0400

    HW1.asm
    First commit

---
+++ HW1.asm
@@ -0,0 +1,17 @@
+# Homework: 1
+# name: GABRIEL_GARRETT
+# sbuid: 108447541
+
+  .text    #MR. T SAYS THIS IS THE TEXT
+  .globl main  #MR. T SAYS THIS IS WHERE THE GLOBAL MAIN STARTS
+.main:  #MR. T SOLEMNLY SWEARS THIS IS MY ARBITRARILY DEFINED MAIN FUNCTION
+  la $a0 intString #Mr. T would like to know which integer you will be entering.
+  
+  
+  
+  
+  .data
+intString: .asciiz "Enter an integer number: "
+  
+  
+  #http://courses.missouristate.edu/kenvollmar/mars/help/syscallhelp.html
