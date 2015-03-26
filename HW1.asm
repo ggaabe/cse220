dcbf1dca7e6e0447fcf77db6573ba3fc10d2abe9
Gabriel Garrett <ggaabe@gmail.com>
2015-03-25 20:03:13 -0400

    Update HW1.asm

--- HW1.asm
+++ HW1.asm
@@ -5,8 +5,10 @@
   .text    #MR. T SAYS THIS IS THE TEXT
   .globl main  #MR. T SAYS THIS IS WHERE THE GLOBAL MAIN STARTS
 .main:  #MR. T SOLEMNLY SWEARS THIS IS MY ARBITRARILY DEFINED MAIN FUNCTION
-  la $a0 intString #Mr. T would like to know which integer you will be entering.
   
+  li $v0, 4  #Mr. T would like to gently load the 4 code into $v0 to prepare for a print string syscall
+  la $a0, intString #Mr. T would like to load the start address of string into the $argument0 register
+  syscall  #Mr. T would to know which integer you would like to enter.
   
   
   
